2b055e486db1ad49544160801d9e09ca101d6a71
Pedro Henrique <pedrohnovais@outlook.com>
2024-07-23 01:16:12 -0300

    Created TaskService class

---
+++ src/main/java/com/pedrosmaxy/todosimple/services/TaskService.java
@@ -0,0 +1,54 @@
+package com.pedrosmaxy.todosimple.services;
+
+import com.pedrosmaxy.todosimple.models.Task;
+import com.pedrosmaxy.todosimple.models.User;
+import com.pedrosmaxy.todosimple.repositories.TaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Service
+public class TaskService {
+
+    @Autowired
+    private TaskRepository taskRepository;
+
+    @Autowired
+    private UserService userService;
+
+    public Task findById(Long id) {
+        Optional<Task> task = taskRepository.findById(id);
+        return task.orElseThrow(() -> new RuntimeException(
+                "Task not found! id: " + id + ", Tipo: " + Task.class.getName()
+        ));
+    }
+
+    @Transactional
+    public Task create(Task task) {
+        User user = this.userService.findById(task.getUser().getId());
+        task.setId(null);
+        task.setUser(user);
+        task = this.taskRepository.save(task);
+        return task;
+    }
+
+    @Transactional
+    public Task update(Task task) {
+        Task newTask = this.findById(task.getId());
+        newTask.setDescription(task.getDescription());
+        return this.taskRepository.save(newTask);
+    }
+
+    public void deleteById(Long id) {
+        findById(id);
+        try {
+            this.taskRepository.deleteById(id);
+        } catch (Exception e) {
+            throw new RuntimeException(
+                    "Not possible to delete because exist related entities!"
+            );
+        }
+    }
+}
